9ea680f79d69ab7c72db1845fd41169eeae271a3
lhf <547389528@qq.com>
2019-06-21 11:18:42 +0800

    6-21-2

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -3,11 +3,14 @@ package com.example.demo.mapper;
 import com.example.demo.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified}) ")
     public void insert(User user);
+    @Select("Select * from user where token =#{token}")
+    User findByToken(@Param("token") String token);
 
 }
